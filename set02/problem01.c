18b445d7bf6d1f2c8511792346abc24b5f583b14
Hussaina  Mustafa <115693609+Hussaina134@users.noreply.github.com>
2022-12-13 06:16:32 +0000

    solved.

--- set02/problem01.c
+++ set02/problem01.c
@@ -8,21 +8,22 @@ int main()
 {
   float base,height;
   float area=0;
-  find_area(base,height,&area);
-  output(base,height,area);
+  input(base,height);
   return 0;
 }
 void input(float base, float height)
 {
+  float area;
   printf("Enter base of triangle: ");
   scanf("%f",&base);
   printf("Enter height of triangle: ");
   scanf("%f",&height);
-  
+  find_area(base,height,&area);
 }
 void find_area(float base , float height, float *area)
 {
   *area = height*base/2;
+  output(base,height,*area);
 }
 void output(float base, float height, float area)
 {
