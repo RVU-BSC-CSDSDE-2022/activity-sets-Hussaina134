1daefcc751823d9a917b29e5120b316ed04c6240
Hussaina  Mustafa <115693609+Hussaina134@users.noreply.github.com>
2022-12-31 15:49:17 +0000

    working on it

--- set02/problem04.c
+++ set02/problem04.c
@@ -5,3 +5,47 @@ int input_array_size();
 void input_array(int n, int a[n]);
 int sum_composite_numbers(int n, int a[n]);
 void output(int sum);
+
+int main()
+{
+  int n,sum;
+  int i;
+  n = input_array_size();
+  input_array(n,a[i]);
+  sum = sum_composite_numbers(n,a[n]);
+  output(sum);
+  return 0;
+}
+int input_array_size()
+{
+  int n;
+  printf("Enter the size of array:");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n, int a[n])
+{
+  int i;
+  for(i=0;i<n;i++){
+    printf("Enter a number:",i+1);
+    scanf("%d",&a[i]);
+  }
+}
+int sum_composite_numbers(int n, int a[n])
+{
+  int i,sum=0;
+  for(i=0;i<n;i++){
+    if(is_composite(a[i])){
+      sum = sum+a[i];
+    }
+  }
+}
+void output(int sum)
+{
+  if(sum==0){
+    printf("No composite numbers in array.");
+  }
+  else{
+    printf("Sum of composite numbers in array is %d",sum);
+  }
+}
