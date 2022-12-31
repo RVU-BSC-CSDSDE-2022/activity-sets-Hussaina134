b64aa714018e155becf9adcaabcf64fb7fc7ed26
Hussaina  Mustafa <115693609+Hussaina134@users.noreply.github.com>
2022-12-31 16:31:46 +0000

    working on it

--- set02/problem06.c
+++ set02/problem06.c
@@ -6,14 +6,30 @@ void str_reverse(char *str, char *rev_str);
 void output(char *a, char *reverse_a);
 
 int main()
-{  
-  char *a;
-  input_string();
+{
+  int i;
+  input_string(&a);
+  str_reverse(&str, &rev_str);
+  output(&a, &reverse_a);
+  return 0;
 }
-
 void input_string(char *a)
 {
-  //char *a;
+  int i;
   printf("Enter a string:\n");
-  scanf("%c",a);
+  scanf("%s",a);
+  //return i;
+}
+void str_reverse(char *str, char *rev_str)
+{
+  int i;
+  for (i = 0; i < strlen(str); i++){
+    rev_str[i] = str[strlen(str)-i-1];
+  }
+  //return rev_str;
+}
+void output(char *a, char *reverse_a)
+{
+  str_reverse(a, reverse_a);
+  printf("The reversed string is %s", reverse_a);
 }
